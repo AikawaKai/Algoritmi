e42783fe08ff370cab965b7106b245a961b83d4b
AikawaKai <marco.odore@studenti.unimi.it>
2015-08-18 15:26:09 +0200

    esercizi

---
+++ lez3/rovescia.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	
+
+	int n=100, i=-1, j,temp;
+	int a[100];
+	printf("Inserisci i numeri da inserire nell'array: ");
+	scanf("%d",&temp);
+	while(temp!=0 && i<100)
+	{	
+		
+		i++;
+		a[i]=temp;
+		scanf("%d",&temp);
+	}
+
+	for(j=i;j>=0;j--)
+	{
+		printf("%d ",a[j]);
+	}
+
+
+}
